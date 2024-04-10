6dd5aab79aa85ce02d0671d50be6251d136efa71
vladkryskiv <148890391+vladkryskiv@users.noreply.github.com>
2024-04-10 19:09:12 +0300

    Add files via upload

---
+++ task4/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+
+interface ISwimmable
+{
+
+    void Swim()
+    {
+        Console.WriteLine("I can swim!");
+    }
+}
+
+// Define an interface IFlyable
+interface IFlyable
+{
+
+    int MaxHeight { get; }
+
+
+    void Fly()
+    {
+        Console.WriteLine($"I can fly at {MaxHeight} meters height!");
+    }
+}
+
+
+interface IRunnable
+{
+
+    int MaxSpeed { get; }
+
+
+    void Run()
+    {
+        Console.WriteLine($"I can run up to {MaxSpeed} kilometers per hour!");
+    }
+}
+
+
+class Animal : ISwimmable, IFlyable, IRunnable
+{
+
+    public int MaxHeight => 100;
+    public int MaxSpeed => 50;
+
+    public void Swim()
+    {
+        Console.WriteLine("I can swim!");
+    }
+
+    public void Fly()
+    {
+        Console.WriteLine($"I can fly at {MaxHeight} meters height!");
+    }
+
+    public void Run()
+    {
+        Console.WriteLine($"I can run up to {MaxSpeed} kilometers per hour!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var animal = new Animal();
+
+
+        animal.Swim();
+        animal.Fly();
+        animal.Run();
+    }
+}
